09e38f72233dc27e20381a7f72deeedf87cb0200
Clemence DE CLERCQ <clemdc1@hotmail.fr>
2023-06-29 15:14:57 +0200

    Création des routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, CocktailContainerComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    CocktailContainerComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
