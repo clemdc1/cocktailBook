dec552edf497df1dc228cb581402cf7e2deb76ea
Clemence DE CLERCQ <clemdc1@hotmail.fr>
2023-06-29 13:53:18 +0200

    Ajout d'une directive

--- src/app/cocktail-container/cocktail-list/cocktail-list.component.ts
+++ src/app/cocktail-container/cocktail-list/cocktail-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Cocktail } from '../../models/cocktail.model';
 import { CommonModule } from '@angular/common';
+import { SelectedDirective } from 'src/app/directives/selected.directive';
 
 @Component({
   selector: 'app-cocktail-list',
   standalone: true,
   templateUrl: './cocktail-list.component.html',
   styleUrls: ['./cocktail-list.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, SelectedDirective],
 })
 export class CocktailListComponent {
   @Input({ required: true }) cocktails!: Cocktail[];
+  @Input({ required: true }) selectedCocktail!: Cocktail;
   @Output() private changeCocktail: EventEmitter<number> =
     new EventEmitter<number>();
   constructor() {}
