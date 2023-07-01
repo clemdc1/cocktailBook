dc5f467833613fcfa4c4ee9265bdafdb459ae8c9
Clemence DE CLERCQ <clemdc1@hotmail.fr>
2023-07-01 10:16:00 +0200

    Utilisation de RestAPI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { APP_ROUTES } from './app.routes';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(APP_ROUTES)],
+  providers: [provideRouter(APP_ROUTES), provideHttpClient()],
 };
--- src/app/services/cocktail.service.ts
+++ src/app/services/cocktail.service.ts
@@ -1,46 +1,18 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Cocktail } from '../models/cocktail.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CocktailService {
-  public cocktails$: BehaviorSubject<Cocktail[]> = new BehaviorSubject([
-    {
-      name: 'Mojito',
-      img: 'https://static.750g.com/images/1200-630/b520523117d647dab6b842a36f4cc7f5/mojito-le-vrai.jpg',
-      description:
-        'The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.',
-      ingredients: [
-        { name: 'Perrier', quantity: 1 },
-        { name: 'Rhum', quantity: 1 },
-        { name: 'Menthe', quantity: 1 },
-      ],
-    },
-    {
-      name: 'Cosmopolitan',
-      img: 'https://assets.afcdn.com/recipe/20180705/80274_w1024h1024c1cx2378cy1278.webp',
-      description:
-        'The tangy concoction of vodka, triple sec, lime juice and cranberry juice has managed to leapfrog the venerable screwdriver as many vodka drinkers prefer the Cosmopolitan’s cleaner and slightly tart taste. The keys to the preparation of a Cosmopolitan are a good brand of cranberry juice and Cointreau Triple Sec, two essential elements to the drink.',
-      ingredients: [
-        { name: 'Cranberry', quantity: 1 },
-        { name: 'Citron', quantity: 1 },
-        { name: 'Vodka', quantity: 1 },
-      ],
-    },
-    {
-      name: 'Mai Tai',
-      img: 'https://www.cocktail.fr/wp-content/uploads/2017/05/mai-tai.jpg',
-      description:
-        'The Mai Tai is a Polynesian-style cocktail that has a fruity tropical taste sweet and vibrant. The mixture of light and dark rum, orange curacao, orgeat syrup and lime juice has been a symbol of Tahitian culture ever since the drink was first created.',
-      ingredients: [
-        { name: 'Rhum', quantity: 1 },
-        { name: 'Triple sec', quantity: 1 },
-        { name: 'Citron', quantity: 1 },
-      ],
-    },
-  ]);
+  constructor(private http: HttpClient) {
+    // this.seed();
+  }
+  public cocktails$: BehaviorSubject<Cocktail[]> = new BehaviorSubject<
+    Cocktail[] | []
+  >([]);
 
   public getCocktail(index: number) {
     return this.cocktails$.value[index];
@@ -61,4 +33,70 @@ export class CocktailService {
       })
     );
   }
+
+  /* public seed() {
+    this.http
+      .get<Cocktail[] | []>('https://restapi.fr/api/cocktails')
+      .subscribe((cocktails: Cocktail[] | []) => {
+        if (!cocktails.length) {
+          this.http
+            .post('https://restapi.fr/api/cocktails', [
+              {
+                name: 'Mojito',
+                img: 'https://static.750g.com/images/1200-630/b520523117d647dab6b842a36f4cc7f5/mojito-le-vrai.jpg',
+                description:
+                  'The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.',
+                ingredients: [
+                  { name: 'Perrier', quantity: 1 },
+                  { name: 'Rhum', quantity: 1 },
+                  { name: 'Menthe', quantity: 1 },
+                ],
+              },
+            ])
+            .subscribe();
+        }
+      });
+    this.http
+      .get<Cocktail[] | []>('https://restapi.fr/api/cocktails')
+      .subscribe((cocktails: Cocktail[] | []) => {
+        if (!cocktails.length) {
+          this.http
+            .post('https://restapi.fr/api/cocktails', [
+              {
+                name: 'Cosmopolitan',
+                img: 'https://assets.afcdn.com/recipe/20180705/80274_w1024h1024c1cx2378cy1278.webp',
+                description:
+                  'The tangy concoction of vodka, triple sec, lime juice and cranberry juice has managed to leapfrog the venerable screwdriver as many vodka drinkers prefer the Cosmopolitan’s cleaner and slightly tart taste. The keys to the preparation of a Cosmopolitan are a good brand of cranberry juice and Cointreau Triple Sec, two essential elements to the drink.',
+                ingredients: [
+                  { name: 'Cranberry', quantity: 1 },
+                  { name: 'Citron', quantity: 1 },
+                  { name: 'Vodka', quantity: 1 },
+                ],
+              },
+            ])
+            .subscribe();
+        }
+      });
+    this.http
+      .get<Cocktail[] | []>('https://restapi.fr/api/cocktails')
+      .subscribe((cocktails: Cocktail[] | []) => {
+        if (!cocktails.length) {
+          this.http
+            .post('https://restapi.fr/api/cocktails', [
+              {
+                name: 'Mai Tai',
+                img: 'https://www.cocktail.fr/wp-content/uploads/2017/05/mai-tai.jpg',
+                description:
+                  'The Mai Tai is a Polynesian-style cocktail that has a fruity tropical taste sweet and vibrant. The mixture of light and dark rum, orange curacao, orgeat syrup and lime juice has been a symbol of Tahitian culture ever since the drink was first created.',
+                ingredients: [
+                  { name: 'Rhum', quantity: 1 },
+                  { name: 'Triple sec', quantity: 1 },
+                  { name: 'Citron', quantity: 1 },
+                ],
+              },
+            ])
+            .subscribe();
+        }
+      });
+  } */
 }
